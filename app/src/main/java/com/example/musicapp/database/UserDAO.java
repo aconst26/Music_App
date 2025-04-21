986eaa7fe5d8bcf0d8aab34b64dfa876c2a533f5
aconst26 <aconst26@gmail.com>
2025-04-20 22:19:53 -0700

    User Entity + UserDAO Fixed Dependencies

---
+++ app/src/main/java/com/example/musicapp/database/UserDAO.java
@@ -0,0 +1,26 @@
+package com.example.musicapp.database;
+
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.musicapp.database.entities.User;
+
+@Dao
+public interface UserDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(User... user);
+
+    @Query("DELETE FROM " + MusicAppDatabase.USER_TABLE)
+    void deleteAll();
+
+    @Query("SELECT * FROM " +  MusicAppDatabase.USER_TABLE +  " WHERE username == :username")
+    LiveData<User> getUserByUserName(String username);
+
+    @Query("SELECT * FROM " +  MusicAppDatabase.USER_TABLE +  " WHERE id == :userId")
+    LiveData<User> getUserByUserId(int userId);
+}
