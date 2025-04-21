7cc64c09f9ba21a9e4ef636d5af93aec366b220b
Edumanu82 <eddiemanu128@gmail.com>
2025-04-20 22:07:43 -0700

    login and MainActivity

---
+++ app/src/main/java/com/example/musicapp/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.example.musicapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
+
+import com.example.musicapp.database.MusicAppRepository;
+import com.example.musicapp.database.entities.User;
+import com.example.musicapp.databinding.ActivityLoginBinding;
+
+public class loginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    private MusicAppRepository repository;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        repository = MusicAppRepository.getRepository(getApplication());
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifyUser();
+
+            }
+        });
+    }
+
+    private void verifyUser() {
+        String username = binding.userNameLoginEditText.getText().toString();
+
+
+        if(username.isEmpty()) {
+            toastMaker("Username should not be blank");
+            return;
+        }
+        LiveData<User> userObserver = repository.getUserByUserName(username);
+        userObserver.observe(this, user -> {
+            if(user != null) {
+                String password = binding.passwordLoginEditText.getText().toString();
+                if(password.equals(user.getPassword())) {
+                    startActivity(MainActivity.mainActivityIntentFactory(getApplicationContext(), user.getId()));
+                } else {
+                    toastMaker("Invalid Password");
+                    binding.passwordLoginEditText.setSelection(0);
+                }
+            } else {
+                toastMaker(String.format("%s is not a valid username.", username));
+                binding.userNameLoginEditText.setSelection(0);
+            }
+        });
+    }
+
+    private void toastMaker(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    static Intent loginIntentFactory(Context context) {
+        return new Intent(context, loginActivity.class);
+    }
+}
+
+
+
+
+
+
+
+
+
