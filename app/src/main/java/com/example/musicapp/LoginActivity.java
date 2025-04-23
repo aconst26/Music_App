6f5282ad5e2256b78e493c7e49261c62f0476863
aconst26 <aconst26@gmail.com>
2025-04-23 13:01:40 -0700

    Fixed admin Page. Admin Button now only displays if user is admin

--- app/src/main/java/com/example/musicapp/LoginActivity.java
+++ app/src/main/java/com/example/musicapp/LoginActivity.java
@@ -36,7 +36,6 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 verifyUser();
-
             }
         });
     }
